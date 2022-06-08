5f54e5909e266a883c706893e669fa4f1bd7aeb6
hasezoey <hasezoey@gmail.com>
2022-06-08 19:02:43 +0200

    feat: add entry point code

---
+++ src/main.ts
@@ -0,0 +1,146 @@
+import * as utils from './utils.js';
+import { promises as fspromises } from 'fs';
+import * as path from 'path';
+
+const log = utils.createNameSpace('main');
+const currentModulePath = utils.getCurrentModuleDirectory(import.meta.url);
+
+// STATIC OPTIONS
+
+/** Project name, which will be used for paths */
+const PROJECT_NAME = 'converty';
+
+/** Converter input path */
+const CONVERTER_READ_PATH = path.resolve(currentModulePath, '../..', `${PROJECT_NAME}-in`);
+/** Converter output path */
+const CONVERTER_OUT_PATH = path.resolve(currentModulePath, '../../', `${PROJECT_NAME}-out`);
+/** Overwrite files to process instead of finding all */
+const OVERWRITE_FILES: undefined | string[] = undefined;
+/** Set "No Module for File" Errors to be Silent */
+const SILENT_NO_MODULE_FOR_FILE: boolean = false;
+/** Set to allow Directories as input to modules, instead of just files */
+const ALLOW_DIR_AS_INPUT: boolean = true;
+
+// CODE
+
+class ModulesError extends Error {
+  constructor(public module: string, public err: Error) {
+    super(`A Error happened with Module "${module}""`);
+  }
+}
+
+async function load_modules(): Promise<utils.ConverterModuleStore[]> {
+  log('Loading Modules');
+  const files = await fspromises.readdir(path.join(currentModulePath, './modules'));
+
+  return Promise.all(
+    files
+      .filter((file) => {
+        // DEBUG: ignore some modules
+        return file.endsWith('.js') && !(file.startsWith('nhentai') || file.startsWith('webtoons'));
+      })
+      .map((file) => {
+        log(`Loading Module "${file}"`);
+
+        const full_file = path.join(currentModulePath, 'modules', file);
+
+        return import(full_file)
+          .then((module) => {
+            const output = module.default();
+
+            utils.assertion(typeof output === 'object', new Error(`Module "${full_file}" default did not return a object`));
+            utils.assertion(typeof output.matcher == 'function', new Error(`Module "${full_file}" did not return a "matcher" function`));
+            utils.assertion(typeof output.process == 'function', new Error(`Module "${full_file}" did not return a "process" function`));
+
+            return { ...output, name: file };
+          })
+          .catch((err) => {
+            throw new ModulesError(file, err);
+          });
+      })
+  );
+}
+
+async function main_loop() {
+  log('Starting Main Loop');
+  const modules = await load_modules();
+
+  if (modules.length === 0) {
+    throw new Error('No Modules');
+  }
+
+  await utils.mkdir(CONVERTER_READ_PATH);
+
+  const finished: string[] = [];
+
+  // call the "ready" function on all modules that have it
+  for (const module of modules) {
+    if (typeof module.ready === 'function') {
+      await module.ready();
+    }
+  }
+
+  console.log('Starting to Process'.grey);
+  log(`READ & OUTPUT Path: "${CONVERTER_READ_PATH}", "${CONVERTER_OUT_PATH}"`);
+
+  // create directories in case they do not exist for future use
+  utils.mkdir(CONVERTER_READ_PATH);
+  utils.mkdir(CONVERTER_OUT_PATH);
+
+  const waitingFiles: Set<string> = new Set();
+
+  for (const foundPath of await fspromises.readdir(CONVERTER_READ_PATH)) {
+    const fullPath = path.resolve(CONVERTER_READ_PATH, foundPath);
+    const stat = await utils.statPath(fullPath);
+
+    // ignore all paths that are not possible to read or are not a file
+    if (utils.isNullOrUndefined(stat) || (!stat.isFile() && !(ALLOW_DIR_AS_INPUT && stat.isDirectory()))) {
+      continue;
+    }
+
+    // skip files if not in "OVERWRITE_FILES", if defined
+    if (!utils.isNullOrUndefined(OVERWRITE_FILES) && !OVERWRITE_FILES.includes(foundPath)) {
+      log(`skipping file "${foundPath}" because its not in OVERWRITE_FILES`);
+      continue;
+    }
+
+    log(`found path: "${fullPath}"`);
+    waitingFiles.add(fullPath);
+  }
+
+  if (waitingFiles.size <= 0) {
+    throw new Error('Found no files to process');
+  }
+
+  for (const file of waitingFiles) {
+    console.log('Processing "'.green + file.grey + '"'.green);
+
+    let processingModel: utils.ConverterModule | undefined = undefined;
+    for (const module of modules) {
+      if (module.matcher(file)) {
+        processingModel = module;
+        break;
+      }
+    }
+
+    // ignore files that dont have a module, but print error
+    if (utils.isNullOrUndefined(processingModel)) {
+      if (!SILENT_NO_MODULE_FOR_FILE) {
+        console.error(new Error(`Could not find a module for path "${file}"`));
+      }
+
+      continue;
+    }
+
+    await processingModel.process({
+      converterInputPath: CONVERTER_READ_PATH,
+      converterOutputPath: CONVERTER_OUT_PATH,
+      fileInputPath: file,
+    });
+
+    console.log('Finished Processing File "'.green + file.grey + '"'.green);
+    finished.push(file);
+  }
+}
+
+main_loop();
---
+++ src/utils.ts
@@ -0,0 +1,290 @@
+import * as https from 'https';
+import { IncomingMessage } from 'http';
+import { promises as fspromises, Stats } from 'fs';
+import debug from 'debug';
+import 'colors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const log = createNameSpace('utils');
+
+export interface ConverterModuleStore extends ConverterModule {
+  /** File Name of the module */
+  name: string;
+}
+
+export interface ConverterModule {
+  /** Function to identify a input for a module */
+  matcher: (name: string) => boolean;
+  /** Function called when everything is ready (before processing any inputs) */
+  ready?: () => Promise<void>;
+  /** Function to process a input with a module */
+  process: (options: ConverterOptions) => Promise<void>;
+}
+
+export interface ConverterOptions {
+  converterInputPath: string;
+  converterOutputPath: string;
+  fileInputPath: string;
+}
+
+/**
+ * "setTimeout" promisified
+ */
+export async function sleep(waitTime: number): Promise<void> {
+  return new Promise((res) => {
+    setTimeout(res, waitTime);
+  });
+}
+
+/**
+ * Assert an condition, if "false" throw error
+ * Note: it is not named "assert" to differentiate between node and jest types
+ * @param cond The Condition to throw
+ * @param error An Custom Error to throw
+ */
+export function assertion(cond: any, error?: Error): asserts cond {
+  if (!cond) {
+    throw error ?? new Error('No Error');
+  }
+}
+
+/**
+ * Assert that "val" is defined (combines "assertions" and "isNullOrUndefined")
+ * @param val The Value to check
+ * @param error Custom Error
+ */
+export function assertionDefined<T>(val: T | undefined | null, error?: Error): asserts val is NonNullable<T> {
+  assertion(!isNullOrUndefined(val), error ?? new Error("Expected 'val' to be defined"));
+}
+
+/**
+ * Because since node 4.0.0 the internal util.is* functions got deprecated
+ * @param val Any value to test if null or undefined
+ */
+export function isNullOrUndefined(val: unknown): val is null | undefined {
+  return val === null || val === undefined;
+}
+
+/** Custom Error to test for to re-try */
+export class DownloadFailedError extends Error {
+  constructor(public url: URL, public code: number, public response: IncomingMessage) {
+    super(`URL "${url.toString()}" failed with code "${code}"`);
+  }
+}
+
+/**
+ * Download given url, without extra processing to the URL
+ * @param url The URL to download
+ * @param extraOptions Extra Options to pass to https
+ * @param withRateLimit Check & Wait for a Rate Limit
+ * @returns Downloaded buffer
+ */
+export async function downloadDirect(url: URL, extraOptions?: https.RequestOptions, withRateLimit?: RateLimit): Promise<Buffer> {
+  return new Promise(async (res, rej) => {
+    if (!isNullOrUndefined(withRateLimit)) {
+      if (withRateLimit.currentCounter >= withRateLimit.maxCounter) {
+        log(`Waiting for RateLimit for "${url.toString()}"`);
+        await withRateLimit.waitfn();
+        log(`Waiting for RateLimit Finished for "${url.toString()}"`);
+      }
+
+      withRateLimit.currentCounter += 1;
+    }
+
+    // Downloading statement put after ratelimit, to make more sense in the log
+    log(`Downloading: "${url.toString()}", with rateLimit: ${!isNullOrUndefined(withRateLimit)}`);
+
+    const httpsOptions = extraOptions ?? {};
+
+    https.get(url.toString(), httpsOptions as any, (response) => {
+      log('Download Status Code: ', response.statusCode);
+
+      if (response.statusCode !== 200) {
+        rej(new DownloadFailedError(url, response.statusCode || 0, response));
+
+        return;
+      }
+
+      const chunks: any = [];
+
+      response.on('data', (chunk) => {
+        chunks.push(chunk);
+      });
+
+      response.on('error', (err) => {
+        rej(err);
+      });
+
+      response.on('end', () => {
+        log(`Download finished for "${url.toString()}"`);
+        res(Buffer.concat(chunks));
+      });
+    });
+  });
+}
+
+/**
+ * Main Download function, follows redirects (up to 5)
+ * @param url The url to download
+ * @param extraOptions Extra Options to pass to https
+ * @param withRateLimit Check & Wait for a Rate Limit
+ * @returns Downloaded buffer
+ */
+export async function download(url: URL, extraOptions?: https.RequestOptions, withRateLimit?: RateLimit): Promise<Buffer> {
+  const actualURL = url;
+  let depth = 0;
+
+  // act like rust's "loop"
+  while (true) {
+    if (depth >= 5 || depth < 0) {
+      throw new Error('Redirect Depth (5) reached');
+    }
+
+    const buff: Buffer | Error | undefined = await downloadDirect(actualURL, extraOptions, withRateLimit).catch((err) => {
+      if (err instanceof DownloadFailedError) {
+        switch (err.code) {
+          case 301:
+          case 302:
+          case 303:
+          case 307:
+          case 308:
+            console.log('TEST REDIRECT'.red, err.response);
+
+            throw new Error('Redirect unimplemented');
+
+            return undefined;
+        }
+      }
+
+      return err;
+    });
+
+    if (isNullOrUndefined(buff)) {
+      depth += 1;
+      continue;
+    }
+
+    if (buff instanceof Error) {
+      throw buff;
+    }
+
+    return buff;
+  }
+}
+
+/**
+ * Apply a "args" to "input" string
+ * @param input The Input which needs to be formatted
+ * @param args The Arguments to format "input" with
+ * @returns The Formatted input
+ */
+export function template(input: string, args: Record<string, any>): string {
+  for (const { 0: key, 1: value } of Object.entries(args)) {
+    log(`Template for key: "${key[0]}"`);
+    input.replaceAll(`<${key.toUpperCase()}>`, value);
+  }
+
+  return input;
+}
+
+export async function mkdir(path: string): Promise<void> {
+  await fspromises.mkdir(path, { recursive: true });
+}
+
+export async function write_file(file: string, content: string | Buffer): Promise<void> {
+  await fspromises.writeFile(file, content);
+}
+
+/**
+ * Run "fs.promises.stat", but return "undefined" if error is "ENOENT" or "EACCES"
+ * follows symlinks
+ * @param path The Path to Stat
+ * @throws if the error is not "ENOENT" or "EACCES"
+ */
+export async function statPath(path: string): Promise<Stats | undefined> {
+  return fspromises.stat(path).catch((err) => {
+    // catch the error if the directory doesn't exist or permission is denied, without throwing an error
+    if (['ENOENT', 'EACCES'].includes(err.code)) {
+      return undefined;
+    }
+
+    throw err;
+  });
+}
+
+/**
+ * Like "fs.existsSync" but async
+ * uses "utils.statPath"
+ * follows symlinks
+ * @param path The Path to check for
+ */
+export async function pathExists(path: string): Promise<boolean> {
+  return !isNullOrUndefined(await statPath(path));
+}
+
+/**
+ * RateLimit, to enforce RateLimits
+ */
+export abstract class RateLimit {
+  public currentCounter: number = 0;
+  public abstract maxCounter: number;
+  public timer?: NodeJS.Timer = undefined;
+
+  public abstract waitfn(): Promise<void>;
+  public abstract reset(): Promise<void>;
+  public abstract createTimer(): Promise<void>;
+
+  public async clearTimer() {
+    if (isNullOrUndefined(this.timer)) {
+      return;
+    }
+
+    while (this.currentCounter > 0) {
+      await this.waitfn();
+    }
+
+    clearInterval(this.timer);
+  }
+}
+
+/**
+ * Basic RateLimit Function to wait for the rate limit (input) to lower
+ * @param time The Time to wait between checks
+ * @param reset The Times it can fail, before calling "reset" on "rl"
+ * @param rl The RateLimit Object
+ */
+export async function defaultRateLimitFn(this: RateLimit, time: number, fail: number): Promise<void> {
+  if (isNullOrUndefined(this.timer)) {
+    await this.createTimer();
+  }
+
+  let tries = 0;
+  while (this.currentCounter >= this.maxCounter) {
+    await sleep(time);
+    tries += 1;
+
+    if (tries >= fail) {
+      // await this.reset();
+      tries = 0;
+    }
+  }
+}
+
+/**
+ * Create a "debug" namespace, without extra imports
+ * @param ns the namespace
+ * @returns a debugger
+ */
+export function createNameSpace(ns: string): debug.Debugger {
+  return debug(`scraper:${ns}`);
+}
+
+/**
+ * Conver the input "import.meta.url" to the dirname
+ * @param currentURL the file url, as returned by "import.meta.url"
+ * @returns the dirname of "import.meta.url" and converted to path
+ */
+export function getCurrentModuleDirectory(currentURL: string): string {
+  return path.dirname(fileURLToPath(currentURL));
+}

b90d6deaff73e785be825d199f742430e1a648e8
hasezoey <hasezoey@gmail.com>
2023-06-15 15:22:29 +0200

    fix(averbil_ln): make use of "generatePElementCombineHook"

--- src/modules/averbil_ln.ts
+++ src/modules/averbil_ln.ts
@@ -67,5 +67,17 @@ export async function process(options: utils.ConverterOptions): Promise<string> 
 
       ssc.genChapterHeaderContent(document, entryType, h1Element);
     },
+    // custom hook to ignore some combining with previous elements, because for example volume 5 chapter 45
+    // has some things which look the same as those that should be combined
+    // and the only differentiator is that those nodes are not just plain text (wrapped in em)
+    generatePElementCombineHook(lastnode, _elems) {
+      for (const child of lastnode.childNodes) {
+        if (child.nodeType !== lastnode.TEXT_NODE) {
+          return true;
+        }
+      }
+
+      return false;
+    },
   });
 }
